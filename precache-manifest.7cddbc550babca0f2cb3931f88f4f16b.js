08abf8706fb9b62eaddcfc76080998f50c07defc
rakeshpandey <rakeshpandey791@gmail.com>
2019-12-14 23:17:12 +0530

    Updates

---
+++ precache-manifest.7cddbc550babca0f2cb3931f88f4f16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ce27bd488811074dbf9efb980da50c",
+    "url": "/bluestack/index.html"
+  },
+  {
+    "revision": "2ff490ae9115a1a632da",
+    "url": "/bluestack/static/css/main.229a3dc1.chunk.css"
+  },
+  {
+    "revision": "c60d922c40f82f7b954c",
+    "url": "/bluestack/static/js/2.5e3de6ca.chunk.js"
+  },
+  {
+    "revision": "2ff490ae9115a1a632da",
+    "url": "/bluestack/static/js/main.70e01522.chunk.js"
+  },
+  {
+    "revision": "1d2b29e8e25047197037",
+    "url": "/bluestack/static/js/runtime-main.605cc936.js"
+  }
+]);
